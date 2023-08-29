ea524537ed139a3e213e52d6153c0926b7ae0d57
Ghiur Tudor Gabriel <73038846+gabighiur@users.noreply.github.com>
2023-08-30 01:06:14 +0300

    add intro-section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,51 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-  /*global */
-  /*eslint no-undef: "error"*/  
-  //const { t, rt  } = useI18n() 
-</script>
-
 <template>
-  <h2>Index</h2>
-  <RouterLink to="/home">Click here</RouterLink>
+  <div class="intro-container" mb-8>
+    <img
+      :src="hyenaScene"
+      alt="NFT Intro Image"
+      class="intro-image"
+    />
+    <div class="light-transition" :style="{ opacity: lightOpacity }"></div>
+    <div class="flex items-center justify-center h-sm absolute inset-0 md:h-full">
+      <TheButton
+        v-if="isDesktop"
+        @mouseenter="handleMouseEnter"
+        @mouseleave="handleMouseLeave"
+        class="enter-button self-end mb-9"
+      >
+      <RouterLink to="/home">Enter</RouterLink>
+      </TheButton>
+      <TheButton
+        v-else
+        class="enter-button self-end mb-8"
+        to="/home"
+      >
+      <RouterLink to="/home">Enter</RouterLink>
+      </TheButton>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const hyenaScene = 'src/assets/hyenaIntro.png';
+const lightOpacity = ref(0);
+
+const handleMouseEnter = () => {
+  lightOpacity.value = 1;
+};
+
+const handleMouseLeave = () => {
+  lightOpacity.value = 0;
+};
+
+const isDesktop = computed(() => window.innerWidth >= 768); 
+</script>
 
 <route lang="yaml">
   meta:
     layout: index
 </route>
+
