466b3e859ee3eba70b98082bff7bd85d35ec991c
unknown <ebu.pro@gmail.com>
2023-09-11 16:59:06 +0200

    set up default dark mode

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,4 +1,10 @@
 // these APIs are auto-imported from @vueuse/core
 export const isDark = useDark()
+
+// Set default to dark mode if no preference is stored yet
+if (typeof localStorage !== 'undefined' && !localStorage.getItem('dark')) {
+    isDark.value = true
+  }
+
 export const toggleDark = useToggle(isDark)
 export const preferredDark = usePreferredDark()
